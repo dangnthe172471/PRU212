9ad5fbd4343dead3f07ebce350a54f36818e532f
dangnthe172471 <dangnthe172471@fpt.edu.vn>
2025-03-08 15:23:20 +0700

    update

--- Project/Assets/Scripts/PlayerCollision.cs
+++ Project/Assets/Scripts/PlayerCollision.cs
@@ -1,18 +1,34 @@
-using UnityEngine;
+﻿using UnityEngine;
 
 public class PlayerCollision : MonoBehaviour
 {
-    [SerializeField] private GameManager gameManager;
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.CompareTag("EnemyBullet"))
-        {
-            Player player = GetComponent<Player>();
-            player.takeDame(20f);
-        }else if (collision.CompareTag("Energy"))
-        {
-            gameManager.addE();
-            Destroy(collision.gameObject);
-        }
-    }
+	[SerializeField] private GameManager gameManager;
+
+	[SerializeField] private AudioManager audioManager;
+
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
+		if (collision.CompareTag("EnemyBullet"))
+		{
+			Player player = GetComponent<Player>();
+			player.takeDame(20f);
+		}
+		else if (collision.CompareTag("Energy"))
+		{
+			gameManager.addE();
+			GameObject[] allGuns = GameObject.FindGameObjectsWithTag("Gun");
+			foreach (var e in allGuns)
+			{
+				Gun gun = e.GetComponent<Gun>();
+				if (gun != null)
+				{
+					gun.ActivateBuff();
+				}
+			}
+
+			Destroy(collision.gameObject);
+
+			audioManager.PlayEnerty();
+		}
+	}
 }
